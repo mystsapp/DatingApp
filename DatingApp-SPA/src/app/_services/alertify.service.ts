0285413b83c3ded4516534302a62481dfeaacd95
ST-PC\ST <vanhongit@gmail.com>
2019-03-11 09:12:08 +0700

    End of section 9

--- DatingApp-SPA/src/app/_services/alertify.service.ts
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -8,28 +8,28 @@ export class AlertifyService {
 
 constructor() { }
 
-confirm(message: string, okCallback: () => any){
-  alertify.confirm(message, function(e){
-    if(e){
+confirm(message: string, okCallback: () => any) {
+  alertify.confirm(message, function(e) {
+    if (e) {
       okCallback();
     } else {}
   });
 }
 
-success(message: string){
-  alertify.success(message)
+success(message: string) {
+  alertify.success(message);
 }
 
-error(message: string){
-  alertify.error(message)
+error(message: string) {
+  alertify.error(message);
 }
 
-warning(message: string){
-  alertify.warning(message)
+warning(message: string) {
+  alertify.warning(message);
 }
 
-message(message: string){
-  alertify.message(message)
+message(message: string) {
+  alertify.message(message);
 }
 
 }
