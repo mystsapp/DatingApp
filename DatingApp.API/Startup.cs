6b15354adaa3da9c6acf5ff8d379e02aa999ecea
ST-PC\ST <vanhongit@gmail.com>
2019-03-17 14:26:42 +0700

    End of section 13

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -54,6 +54,7 @@ namespace DatingApp.API {
                         ValidateAudience = false
                     };
                 });// for Authorize
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
