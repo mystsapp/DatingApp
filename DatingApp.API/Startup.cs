5c4770ae7d9731e0faf1a27bbfefba3ea87cbcd1
ST-PC\ST <vanhongit@gmail.com>
2019-03-26 10:05:48 +0700

    End of section 17

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -15,22 +15,54 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+// using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
+
 namespace DatingApp.API {
     public class Startup {
         public Startup (IConfiguration configuration) {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<DataContext> (x => x
+                .UseMySql (Configuration.GetConnectionString ("DefaultConnection"))
+                .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning)));
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1)
+            .AddJsonOptions(opt => {
+                opt.SerializerSettings.ReferenceLoopHandling = 
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            }); // ignore some error
+            services.AddCors (); //for sharing webapi
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+            services.AddAutoMapper(); // automapper
+            services.AddTransient<Seed>(); //add users into database
+            services.AddScoped<IAuthRepository, AuthRepository> (); //register service
+            services.AddScoped<IDatingRepository, DatingRepository> (); //register service
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (options => {
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey (Encoding.ASCII
+                                .GetBytes (Configuration.GetSection ("AppSettings:Token").Value)),
+                        
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });// for Authorize
+            services.AddScoped<LogUserActivity>();
+        }
+
+        public void ConfigureDevelopmentServices (IServiceCollection services) {
             services.AddDbContext<DataContext> (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
             services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1)
             .AddJsonOptions(opt => {
@@ -78,9 +110,25 @@ namespace DatingApp.API {
 
             // app.UseHttpsRedirection();
             //seeder.SeedUsers(); //add users into database
-            app.UseCors (x => x.AllowAnyOrigin ().AllowAnyHeader ().AllowAnyMethod ());// allow arigin
+            //app.UseCors (x => x.AllowAnyOrigin ().AllowAnyHeader ().AllowAnyMethod ());// allow arigin
+            app.UseCors(x => x.WithOrigins("http://localhost:4200")
+                .AllowAnyMethod().AllowAnyHeader().AllowCredentials());
             app.UseAuthentication();
-            app.UseMvc ();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();            
+            // app.UseMvc (routes => {
+            //     routes.MapSpaFallbackRoute(
+            //      name: "spa-fallback",
+            //      defaults: new { controller = "Fallback", action = "Index"}
+            //     );
+            // });
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
+////////// loi seed users nhieu lan
